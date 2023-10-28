aeba0d71502d32d54269e15ed5f5a9ff52e5adb4
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-28 21:05:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function onesDigit(n) {
   // your code here
+	if(n > 0)
+	{
+		let onesDigit = n % 10;
+		return onesDigit;
+	}
 }
 
 // Do not change the code below
-
 const n = prompt("Enter Number:");
 alert(onesDigit(n));
+
+
